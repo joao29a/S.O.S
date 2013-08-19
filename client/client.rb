c3074f1dc262fc651b0cdf70b35750852a0a7a7d
tyurok <tyurok_slayer89@hotmail.com>
2013-08-19 19:04:28 -0300

    Lendo sobre Socket

---
+++ client/client.rb
@@ -0,0 +1,23 @@
+require 'socket'
+
+def Menu()
+    puts("1 - Cadastrar novo site:")
+    puts("2 - Buscar site: ")
+    puts("3 - Sair: ")
+    opcao = gets.chomp
+end
+
+def Cadastrar()
+    echo("clear")
+    puts("Insira o nome do site: ")
+    nomeSite = gets.chomp
+    puts("Insira o endereco do site: ")
+    link = gets.chomp
+    puts("Tags relacionadas ao site: (Insira as Tags separadas por virgula) ")
+    tags = gets.chomp
+    #for i in tags
+end
+
+def Buscar()
+    puts("Insira uma palavra-chave para busca: ")
+    
