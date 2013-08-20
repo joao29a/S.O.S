d1231e555f5550fb7fb951c9be8fd948c45c7af8
Diogo Murata <diogomurat@gmail.com>
2013-08-20 08:57:49 -0300

    Conecta no Socket

--- client/client.rb
+++ client/client.rb
@@ -1,22 +1,34 @@
 require 'socket'
-#http://www.ruby-doc.org/stdlib-1.9.3/libdoc/socket/rdoc/Socket.html
+
+
 def Menu()
     puts("1 - Cadastrar novo site:")
     puts("2 - Buscar site: ")
     puts("3 - Sair: ")
     opcao = gets.chomp
+    if (opcao == "1")
+        Cadastrar()
+    elsif (opcao == "2")
+        Buscar()
+    else
+        puts("Adios!")
+    end
 end
 
 def Cadastrar()
-    echo("clear")
+    system "clear"
+    socket = TCPSocket.new 'localhost', 8888
     puts("Insira o nome do site: ")
-    nomeSite = gets.chomp
+    nomeSite = socket.gets.chomp
     puts("Insira o endereco do site: ")
-    link = gets.chomp
+    link = socket.gets.chomp
     puts("Tags relacionadas ao site: (Insira as Tags separadas por virgula) ")
-    tags = gets.chomp
+    tags = socket.gets.chomp
 end
 
-def Buscar()
-    puts("Insira uma palavra-chave para busca: ")
-    
+#def Buscar()
+#    puts("Insira uma palavra-chave para busca: ")
+
+
+Menu()
+
