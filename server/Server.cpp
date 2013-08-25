ec35dc5a1f8c605864b3a210575b421f136171f9
John <john@john-programmer.(none)>
2013-08-25 19:36:37 -0300

    Semáforos implementado, mas ainda não ta funcionando legal.

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,16 +1,38 @@
 #include "include/Server.h"
 
+void* Server::getMemoryAddr(){
+	return memoryRegion->get_address();
+}
+
+void Server::createMessageQueue(){
+	shm = new (getMemoryAddr()) shared_messages;
+}
+
 void Server::createSharedMemory(){
 	try{
 		memoryRegion = new mapped_region(anonymous_shared_memory(MEMORY_SIZE));
 		memset(memoryRegion->get_address(),0,memoryRegion->get_size());
+		createMessageQueue();
 	} catch (exception& e){
 		cerr << "Exception: sharedMemory: " << e.what() << endl;
 	}
 }
 
 void Server::manipulateData(string data, socket_ptr sock){
-	messageQueue.push_back(data);
+	shm->mutex.wait();
+	shm->messageQueue.push_back(data);
+	shm->size.post();
+	shm->mutex.post();
+}
+
+void Server::consumeData(){
+	while(true){
+		shm->mutex.wait();
+		shm->size.wait();
+		cout << shm->messageQueue.front() << endl;
+		shm->messageQueue.pop_front();
+		shm->mutex.post();
+	}
 }
 
 void Server::execMsg(string msg){
@@ -23,6 +45,7 @@ void Server::initServer(int port){
 		initSocket(port);
 	}
 	else if (id == 0){
+		consumeData();
 	}
 }
 
--- server/include/Server.h
+++ server/include/Server.h
@@ -5,20 +5,31 @@
 #include <list>
 #include <boost/interprocess/anonymous_shared_memory.hpp>
 #include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
 #define MEMORY_SIZE 10000
 
 using namespace boost::interprocess;
 
+struct shared_messages{
+	shared_messages() : mutex(1), size(0){}
+	interprocess_semaphore mutex, size;
+	list<string> messageQueue;
+};
+
 class Server: public Management{
 	private:
 		mapped_region* memoryRegion;
+		shared_messages* shm;
+		void* getMemoryAddr();
+		void createMessageQueue();
 		void createSharedMemory();
+		void consumeData();
 
 	protected:
-		list<string> messageQueue;
 		void manipulateData(string,socket_ptr);
 		void execMsg(string);
+
 	public:
 		void initServer(int);
 };
