099c270a3a3b9743b297a4998b13dafbaf9f0a59
MurkyDTM <diogomurat@gmail.com>
2013-08-29 16:28:43 -0300

    wtf

--- client/client.rb
+++ client/client.rb
@@ -1,44 +1,76 @@
 require 'socket'
 
+=begin
+require 'gtk2'
+
+class RubyApp < Gtk::Window
+
+    def initialize
+        super
+    
+        set_title "Center"
+        signal_connect "destroy" do 
+            Gtk.main_quit 
+        end
+
+        set_default_size 250, 200
+        set_window_position Gtk::Window::POS_CENTER
+        
+        show
+    end
+end
+
+Gtk.init
+    window = RubyApp.new
+Gtk.main
+=end
 
 def Menu()
-    puts("1 - Cadastrar novo site:")
-    puts("2 - Buscar site: ")
-    puts("3 - Sair: ")
+    puts("Sistema de Cadastro de Especies de Animais")
+    puts("1 - Cadastrar nova especie:")
+    puts("2 - Buscar especie: ")
+    puts("3 - Remover: ")
+    puts("4 - Sair: ")
     opcao = gets.chomp
     if (opcao == "1")
         Cadastrar()
     elsif (opcao == "2")
         Buscar()
+    elsif (opcao == "3")
+        Remover()
     else
-        puts("Adios!")
+        puts("Volte Sempre!")
     end
 end
 
 def Cadastrar()
     system "clear"
     socket = TCPSocket.new 'localhost', 8888
-    puts("Insira o nome do site: ")
-    nomeSite = gets.chomp
-    socket.send "#{nomeSite}", 0
-    puts("Insira o endereco do site: ")
-    link = gets.chomp
-    puts("Tags relacionadas ao site: (Insira as Tags separadas por virgula) ")
-    tags = gets.chomp
+    puts("Insira o ID da especie: ")
+    id = gets.chomp
+    puts("Insira o Nome Cientifico da Especie: ")
+    nomeCientifico = gets.chomp
+    puts("Insira o Nome Popular da Especie: ")
+    nomePopular = gets.chomp
+    puts("Insira uma breve descricao da Especie: ")
+    breveDesc = gets.chomp
+    socket.send "cadastrar?id=#{id}&nomeCientifico=#{nomeCientifico}&nomePopular=#{nomePopular}&breveDesc=#{breveDesc}", 0
 end
 
-=begin
 def Buscar()
-    puts("Insira uma palavra-chave para busca: ")
+    socket = TCPSocket.new 'localhost', 8888
+    puts("Insira uma id para busca: ")
     busca = gets.chomp
-    for i in bancoDados 
-        if (i == busca)
-            encontrado = i + ","
-        else
-            puts("O termo desejado nao foi encontrado nos Servidores: ")
-        end
-    end
-=end
+    socket.send "buscar?id=#{busca}", 0
+    puts(socket.gets)
+end
+
+def Remover()
+    socket = TCPSocket.new 'localhost', 8888
+    puts("Insira o id da especie que deseja remover: ")
+    remove = gets.chomp
+    socket.send "remover?id=#{remove}", 0
+end
 
 Menu()
 
